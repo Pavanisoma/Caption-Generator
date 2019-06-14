ffcb18f269455afa13463505207365a7b9c961b5
Pavanisomarouthu <42732676+Pavanisomarouthu@users.noreply.github.com>
2019-06-13 20:43:46 -0700

    Add files via upload

--- sequence_utils.py
+++ sequence_utils.py
@@ -13,10 +13,13 @@ from keras.models import load_model
 def get_all_desc(description_dict):
     all_desc = list()
     for key in description_dict.keys():
-        [all_desc.append(d) for d in description_dict[key]]
+        for d in description_dict[key]:
+            # d = 'startseq ' + d + ' endseq'
+            all_desc.append(d)
+        # [all_desc.append(d) for d in description_dict[key]]
     return all_desc
  
-# fit a tokenizer given caption descriptions
+# fit a tokenizer given caption descriptions    
 # get the word counts of the vocabulary
 def create_tokenizer(description_dict):
     lines = get_all_desc(description_dict)
@@ -58,45 +61,6 @@ def create_embedding_matrix(tokenizer, embeddings_index):
     return embedding_matrix
 
 
-# def testing(caption,weight, img_dir, beam_size=3):
-#     captions = {}
-#     with open(img_dir, 'r') as images_path:
-#         images = images_path.read().strip().split('\n')
-#     encoded_images = pickle.load(open("encoded_test_images_inceptionv3.p", "rb"))
-#     #model = e.create_model(ret_model=True)
-#     #model.load_weights(weight)
-
-#     prediction = open('predicted_captions.txt', 'w')
-#     for count, image in enumerate(images):
-#         image = encoded_images[image]
-#         captions[image] = generate_desc(model, tokenizer, test_features_dict['3385593926_d3e9c21170'].reshape((1,2048)), max_length)
-#         prediction.write(image + "\t" + str(caption))
-#         prediction.flush()
-#     prediction.close()
-
-#     captions_path = open('Flickr8k_text/Flickr8k.token.txt', 'r')
-#     captions_text = captions_path.read().strip().split('\n')
-#     cap_pair = {}
-#     for i in captions_text:
-#         i = i.split("\t")
-#         i[0] = i[0][:len(i[0]) - 2]
-#         try:
-#             cap_pair[i[0]].append(i[1])
-#         except:
-#             cap_pair[i[0]] = [i[1]]
-#     captions_path.close()
-
-#     h = []
-#     r = []
-#     for image in images:
-#         h.append(captions[image])
-#         r.append(cap_pair[image])
-
-#     return BLEU(h, r)
-
-# def BLEU(h, r):
-#     return translate.bleu_score.corpus_bleu(r, h)
-
 def main():
     # sample data
     train_description_dict = { '2671602981_4edde92658': ['a little girl in a bathing suit leaps up in the water', 
@@ -104,8 +68,10 @@ def main():
     'a young girl in a swimming suit jumps into a body of water', 
     'a young girl jumping out of the water', 
     'the girl in the bathing suit is poised in midair next to the blue water']}
-    tokenizer = create_tokenizer(train_description_dict)
-    print(tokenizer.texts_to_sequences(['a young girl']))
+   # tokenizer = create_tokenizer(train_description_dict)
+    #print(tokenizer.texts_to_sequences(['a young girl']))
+
 
 if __name__ == '__main__':
    main()
+
